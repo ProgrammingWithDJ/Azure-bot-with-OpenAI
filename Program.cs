e9ad0c8d468f2babae9a02c1dffc14bae30faf5b
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2023-03-05 13:48:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+
+namespace Speechtotext
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            await SynthesizeAudioAsync();
+        }
+
+        private static async Task SynthesizeAudioAsync()
+        {
+            var config = SpeechConfig.FromSubscription("", "");
+           
+
+            using (var recog= new SpeechRecognizer(config))
+            {
+                Console.WriteLine("Speak");
+
+                var result = await recog.RecognizeOnceAsync();
+
+                if(result.Reason ==ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine(result.Text);
+                }
+
+                config.SpeechSynthesisVoiceName = "en-GB-RyanNeural";
+                using var synthesizer = new SpeechSynthesizer(config);
+                Console.WriteLine("Your robot will work now");
+                await synthesizer.SpeakTextAsync(result.Text);
+            }
+
+
+            
+        }
+    }
+}
